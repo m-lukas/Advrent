269e8f8fd8f9f4ee259c02e959b200bb5fcaea2f
m-lukas <mail@lukasmueller.de>
2021-11-30 18:51:08 +0100

    Stop splashscreen and add icon

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
 import Wall from './screens/Wall';
 import Content from './screens/Content';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
